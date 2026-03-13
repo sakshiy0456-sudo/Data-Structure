3131ebaea8c43a23fd5868d9d4f3c941c6368ba9
sakshiy0456-sudo <sakshiy0456@gmail.com>
2026-03-13 11:21:04 +0530

    Add marks and update age in student dictionary
    Added a 'marks' key to the student dictionary and updated the 'age' value.

---
+++ dictionary.py
@@ -0,0 +1,14 @@
+ 1st operation : Added a column to our dictionary
+student={
+    "name":"Tanishq Singh",
+    "age":18,
+    "course":"BCA"
+}
+print("Original Dictionary:",student)
+student["marks"]=85
+print("After adding marks:",student)
+
+# 2nd operation: Updating a value in our dictionary
+
+student["age"]=19
+print("After updating age:",student)
