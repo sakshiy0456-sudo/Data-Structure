84a7dd6691727b3528be94b739d7baeb54cbc50b
sakshiy0456-sudo <sakshiy0456@gmail.com>
2026-03-13 11:22:18 +0530

    Implement basic stack operations in stack.py

---
+++ stack.py
@@ -0,0 +1,11 @@
+stack=[]
+
+stack.append(10)
+stack.append(20)
+stack.append(30)
+print("Stack after pushing elements:",stack)
+
+if len(stack)==0:
+    print("Stack is empty")
+else:
+    print("Stack is not empty")
