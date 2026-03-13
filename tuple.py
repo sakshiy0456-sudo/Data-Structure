be8f817958313acd0e54a8f14b055e14ad406a84
sakshiy0456-sudo <sakshiy0456@gmail.com>
2026-03-13 11:21:42 +0530

    Add tuple operations for length and slicing

---
+++ tuple.py
@@ -0,0 +1,8 @@
+tup=(10,20,30,40,50)
+print("Original Tuple:",tup)
+
+# 1st operation: measuring the length of tuple
+print("Length of tuple:",len(tup))
+
+# 2nd operation: Slicing the tuple
+print("Elements from index 1 to 3:",tup[1:4])
